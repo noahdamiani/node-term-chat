0d6a6c808dce45aff26c405902392d8ecea1a107
noahdamiani <noah@bloombox.io>
2019-01-22 15:09:36 -0800

    init

---
+++ client/client.js
@@ -0,0 +1,60 @@
+const io = require('socket.io-client');
+const readline = require('readline');
+const chalk = require('chalk');
+const argv = require('minimist')(process.argv.slice(2));
+
+readline.emitKeypressEvents(process.stdin);
+process.stdin.setRawMode(true);
+
+class Client {
+  constructor() {
+    this.chat = io.connect(argv.server);
+    this.user = 'Anonymous';
+    this.rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+
+    this.chat.on('connect', () => this.init())
+    this.chat.on('chat message', (msg) => {
+      if(msg.split(": ").length > 1) {
+        let sp = msg.split(": "),
+          user = sp[0],
+          text = sp[1]
+        console.log(`${chalk.bgWhite(chalk.bold(chalk.gray(` ${user} `)))}: ${chalk.blue(text)}`);
+      } else {
+        console.log(`${chalk.bgYellow(chalk.bold(chalk.gray(` ${msg} `)))}`)
+      }
+      this.message();
+    })
+  }
+
+  init() {
+    this.setUser(() => {
+      this.chat.emit('chat message', `${this.user} joined.`)
+    });
+  }
+
+  setUser(callback) {
+    if(argv.username) {
+      this.user = argv.username
+    } else {
+      this.rl.question('username?  ', (username) => {
+        this.user = username;
+      });
+    }
+
+    callback();
+    this.message();
+    this.rl.clearLine();
+    process.stdout.cursorTo(0)
+  }
+
+  message() {
+    this.rl.question('>> ', (answer) => {
+      this.chat.emit('chat message', `${this.user}: ${answer}`);
+    });
+  }
+}
+
+new Client();
